41f7b804b262029151b8270d91f13836756dff8b
Marek <nphx@outlook.com>
2015-07-16 15:36:33 +0200

    Add a simple template for testing

--- src/Templatus/Program.fs
+++ src/Templatus/Program.fs
@@ -1,3 +1,12 @@
-﻿[<EntryPoint>]
-let main _ = 
-    0
+﻿namespace Templatus
+
+open Templatus.Core
+
+module Main =
+    [<EntryPoint>]
+    let main argv = 
+        match argv.Length with
+        | 0 -> eprintfn "No file provided"; 1
+        | _ -> match TemplateParser.parse argv.[0] with
+               | Success result -> printfn "%A" result; 0
+               | Failure reason -> eprintfn "Template parsing failed: %s" reason; 2
