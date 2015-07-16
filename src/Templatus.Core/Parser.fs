bcbe71846456fd0fa485277f07489c5732d6c94c
Marek <nphx@outlook.com>
2015-07-16 15:10:24 +0200

    Add basic parser

---
+++ src/Templatus.Core/Parser.fs
@@ -0,0 +1,38 @@
+﻿namespace Templatus.Core
+
+open FParsec
+
+type ParsingResult =
+    | Success of TemplatePart list
+    | Failure of string
+
+module TemplateParser =
+    let maxCharsTillString str skipString =
+        charsTillString str skipString System.Int32.MaxValue
+
+    let pAssemblyReference: Parser<Directive, string> =
+        skipString "assembly" >>. spaces1 >>. skipString "assembly=\"" >>. maxCharsTillString "\"" true |>> AssemblyReference
+
+    let pInclude: Parser<Directive, string> =
+        skipString "include" >>. spaces1 >>. skipString "file=\"" >>. maxCharsTillString "\"" true |>> Include
+
+    let pOutput: Parser<Directive, string> =
+        skipString "output" >>. spaces1 >>. skipString "filename=\"" >>. maxCharsTillString "\"" true |>> Output
+
+    let pDirective: Parser<TemplatePart, string> =
+        let pDirectiveType = choice [ pAssemblyReference; pInclude; pOutput ]
+        skipString "<#@" >>. spaces >>. pDirectiveType .>> spaces .>> skipString "#>" |>> DirectivePart
+
+    let pLiteral: Parser<TemplatePart, string> = 
+        maxCharsTillString "<#" false |>> Literal |>> LiteralPart
+
+    let pControl: Parser<TemplatePart, string> =
+        skipString "<#" >>. maxCharsTillString "#>" true |>> Control |>> ControlPart
+
+    let pTemplate: Parser<TemplatePart list, string> =
+        many (choice [ pDirective; pControl; pLiteral; ]) .>> eof
+
+    let parse filePath =
+        match runParserOnFile pTemplate "" filePath System.Text.UTF8Encoding.UTF8 with
+        | ParserResult.Success (parsed, _, _) -> Success parsed
+        | ParserResult.Failure (reason, _, _) -> Failure reason
---
+++ src/Templatus.Core/TemplateParts.fs
@@ -0,0 +1,15 @@
+﻿namespace Templatus.Core
+
+type Directive =
+    | AssemblyReference of string
+    | Include of string 
+    | Output of string
+
+type Literal = Literal of string
+
+type Control = Control of string
+
+type TemplatePart =
+    | DirectivePart of Directive
+    | LiteralPart of Literal
+    | ControlPart of Control
