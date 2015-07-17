2122217c6abae7cd87408b7fc975cd500f628d4c
Marek <nphx@outlook.com>
2015-07-17 16:19:25 +0200

    Add output generation

--- src/Templatus.Core/OutputGenerator.fs
+++ src/Templatus.Core/OutputGenerator.fs
@@ -1,18 +1,35 @@
 ﻿namespace Templatus.Core
 
+open System
 open System.IO
 open System.Text
+open System.Text.RegularExpressions
 open Chessie.ErrorHandling
 open Microsoft.FSharp.Compiler.Interactive.Shell
 
 module OutputGenerator =
-    let prep outputFileName = [
-        "open System;;"
-        "let templateOutputFile = new System.IO.StreamWriter \"" + outputFileName + "\";;"
-        "let tprintf o = sprintf \"%O\" o |> templateOutputFile.Write;;"
-        "let tprintfn o = sprintf \"%O\" o |> templateOutputFile.WriteLine;;" ]
+    let private prep outputFileName = [
+        "open System"
+        "let templateOutputFile = new System.IO.StreamWriter \"" + outputFileName + "\""
+        "let tprintf o = sprintf \"%O\" o |> templateOutputFile.Write"
+        "let tprintfn o = sprintf \"%O\" o |> templateOutputFile.WriteLine" ]
 
-    let finish = [ "templateOutputFile.Close ();;" ]
+    let private finish = [ "templateOutputFile.Close ()" ]
+
+    let private prepareControlPart (text: string) =
+        text.Replace("\t", "    ") // FSI complains about tabs
+
+    let private normalizeRegex = new Regex (@"\r\n|\n\r|\n|\r", RegexOptions.Compiled)
+
+    let private prepareLiteralPart text =
+        normalizeRegex.Replace(text, Environment.NewLine) |> sprintf "tprintf \"%s\""
+
+    let private prepareTemplateForEval processedTemplate =
+        processedTemplate.FilteredTemplateParts
+        |> List.map (fun p -> match p with
+                              | LiteralPart (Literal l) -> prepareLiteralPart l
+                              | ControlPart (Control c) -> prepareControlPart c
+                              | DirectivePart _ -> failwith "DirectivePart not filtered out")
 
     let generate processedTemplate =
         let sbOut = StringBuilder ()
@@ -22,7 +39,11 @@ module OutputGenerator =
         let fsi = FsiEvaluationSession.Create(cfg, [|"--noninteractive"|], new StringReader (""), new StringWriter (sbOut), new StringWriter (sbErr))
 
         prep processedTemplate.Directives.Output.Head |> List.iter fsi.EvalInteraction
-        fsi.EvalInteraction """tprintfn "Hello from OutputGenerator!";;"""
+
+        try
+            processedTemplate |> prepareTemplateForEval |> List.iter fsi.EvalInteraction
+        with _ -> ()
+
         finish |> List.iter fsi.EvalInteraction
 
         let errors = sbErr.ToString ()
