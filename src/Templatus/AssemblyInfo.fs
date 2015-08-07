c94cdd298556d1e5241aea4fa119944731c15bcd
kerams <nphx@outlook.com>
2015-08-07 11:09:12 +0200

    Release version 0.1.2

--- src/Templatus/AssemblyInfo.fs
+++ src/Templatus/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("Templatus command line")>]
 [<assembly: GuidAttribute("1a2d4fdb-650e-48c5-abb8-a5348329811b")>]
 [<assembly: AssemblyProductAttribute("Templatus")>]
-[<assembly: AssemblyDescriptionAttribute("T4-like templating tool")>]
-[<assembly: AssemblyVersionAttribute("0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1")>]
-[<assembly: AssemblyMetadataAttribute("githash","def613d855f17c3f08048baf4ce62139a6e4bba9")>]
+[<assembly: AssemblyDescriptionAttribute("T4-like templating tool with support for F#")>]
+[<assembly: AssemblyVersionAttribute("0.1.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.2")>]
+[<assembly: AssemblyMetadataAttribute("githash","bbf11a8063c7a054aec5d77824e9bf2a49f4d0a7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1"
+    let [<Literal>] Version = "0.1.2"
