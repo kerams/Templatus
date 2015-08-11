28a0691b605e3d191a05102e542254334da8c957
Marek <nphx@outlook.com>
2015-08-11 11:41:48 +0200

    Add option to parallelize template processing

--- src/Templatus/Program.fs
+++ src/Templatus/Program.fs
@@ -6,14 +6,16 @@ open Chessie.ErrorHandling
 open Nessos.UnionArgParser
 
 type Args =
-    | [<AltCommandLine("-t")>] Templates of string
-    | [<CustomCommandLine("--params")>][<AltCommandLine("-p")>] TemplateParameters of string
+    | [<CustomCommandLine("-t")>] Templates of string
+    | [<CustomCommandLine("-p")>] TemplateParameters of string
+    | [<CustomCommandLine("-parallelization")>] Parallelization of int
 with
     interface IArgParserTemplate with
         member s.Usage =
             match s with
             | Templates _ -> "path to the template to process"
             | TemplateParameters _ -> "parameters to pass in the templates, i.e. --params age=3;name=Timmy"
+            | Parallelization _ -> "degree of parallelism of template processing"
 
 module Main =
     let getTemplateNames (parsedArgs: ArgParseResults<Args>) =
@@ -29,15 +31,31 @@ module Main =
                              |> List.choose (fun ps -> if ps.Length <> 2 then None else Some (ps.[0], ps.[1]))
         | None -> []
 
+    let getDegreeOfParallelism (parsedArgs: ArgParseResults<Args>) =
+        match parsedArgs.TryGetResult <@ Parallelization @> with
+        | Some n -> if n < 1 then 1 else n
+        | None -> 1
+
+    open Chessie.ErrorHandling.AsyncExtensions
+
     [<EntryPoint>]
     let main _ =
         let results = UnionArgParser.Create<Args>().Parse(ignoreUnrecognized = true, raiseOnUsage = false)
+        let parameters = getTemplateParameters results
+        let parallelism = getDegreeOfParallelism results
+
+        let processChunk list = async {
+            return list |> List.map TemplateParser.parse
+                   |> List.map (bind (Processor.processTemplate TemplateParser.parse))
+                   |> List.map (bind (OutputGenerator.generate parameters)) }
 
         let createOutput = results |> getTemplateNames
                            >>= Utils.checkTemplatesExist
-                           >>= (List.map TemplateParser.parse >> collect)
-                           >>= (List.map (Processor.processTemplate TemplateParser.parse) >> collect) 
-                           >>= (List.map (OutputGenerator.generate (getTemplateParameters results)) >> collect)
+                           |> lift (List.splitInto parallelism)
+                           |> lift (List.map processChunk)
+                           |> lift (Async.Parallel >> Async.RunSynchronously)
+                           |> lift (List.ofArray >> List.concat)
+                           >>= collect
 
         match createOutput with
         | Ok _ -> 0
