fad3d11c001716474cb22e3dc4f3e7267cde2eb2
kerams <nphx@outlook.com>
2016-01-24 13:26:31 +0100

    Minor changes to CLI syntax

--- src/Templatus/Program.fs
+++ src/Templatus/Program.fs
@@ -5,30 +5,27 @@ open Chessie.ErrorHandling
 open Argu
 
 type Args =
-    | [<CustomCommandLine("-t")>] Templates of string
-    | [<CustomCommandLine("-p")>] TemplateParameters of string
+    | [<CustomCommandLine("-t")>] Template of string
+    | [<CustomCommandLine("-p")>][<Rest>] TemplateParameters of string
     | [<CustomCommandLine("-parallelization")>] Parallelization of int
 with
     interface IArgParserTemplate with
         member s.Usage =
             match s with
-            | Templates _ -> "path to the template to process"
+            | Template _ -> "path to the template to process"
             | TemplateParameters _ -> "parameters to pass in the templates, i.e. --params age=3;name=Timmy"
             | Parallelization _ -> "degree of parallelism of template processing"
 
 module Main =
     let getTemplateNames (parsedArgs: ParseResults<Args>) =
-        match parsedArgs.GetResults <@ Templates @> with
-        | [] -> parsedArgs.Usage (message = "No templates provided.\nUsage:") |> fail
-        | list -> pass list
+        match parsedArgs.TryGetResult <@ Template @> with
+        | Some t -> t.Split ';' |> List.ofArray |> pass
+        | None -> parsedArgs.Usage (message = "No templates provided.\nUsage:") |> fail
 
     let getTemplateParameters (parsedArgs: ParseResults<Args>) =
-        match parsedArgs.TryGetResult <@ TemplateParameters @> with
-        | Some parameters -> parameters.Split ';'
-                             |> List.ofArray
-                             |> List.map (fun p -> p.Split '=')
-                             |> List.choose (fun ps -> if ps.Length <> 2 then None else Some (ps.[0], ps.[1]))
-        | None -> []
+        parsedArgs.GetResults <@ TemplateParameters @>
+        |> List.map (fun p -> p.Split '=')
+        |> List.choose (fun ps -> if ps.Length <> 2 then None else Some (ps.[0], ps.[1]))
 
     let getDegreeOfParallelism (parsedArgs: ParseResults<Args>) =
         match parsedArgs.TryGetResult <@ Parallelization @> with
@@ -37,19 +34,19 @@ module Main =
 
     [<EntryPoint>]
     let main _ =
-        let results = ArgumentParser.Create<Args>().Parse(ignoreUnrecognized = true, raiseOnUsage = false)
-        let parameters = getTemplateParameters results
-        let parallelism = getDegreeOfParallelism results
+        let parsedArgs = ArgumentParser.Create<Args>().Parse(ignoreUnrecognized = true, raiseOnUsage = false)
+        let parameters = getTemplateParameters parsedArgs
+        let parallelism = getDegreeOfParallelism parsedArgs
 
         let processList = List.map TemplateParser.parse
                           >> List.map (bind (Processor.processTemplate TemplateParser.parse))
                           >> List.map (bind (OutputGenerator.generate parameters))
                           >> Async.singleton
-                   
+
         printfn "Degree of parallelism: %d" parallelism
         printfn "Starting..."
 
-        let createOutput = results |> getTemplateNames
+        let createOutput = parsedArgs |> getTemplateNames
                            >>= Utils.checkTemplatesExist
                            |> lift (List.splitInto parallelism)
                            |> lift (List.map processList)
